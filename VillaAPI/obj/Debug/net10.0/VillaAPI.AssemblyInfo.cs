6e336280f9adf433e5acaa095cbda6f6dae0aeba
ArielHayon <arielhayon14@gmail.com>
2025-12-01 18:08:53 +0200

    leasson 41 and dependansy injection

--- VillaAPI/obj/Debug/net10.0/VillaAPI.AssemblyInfo.cs
+++ VillaAPI/obj/Debug/net10.0/VillaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VillaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+888910dc1c83391ddd2dd66a2e5caea0429fac9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840a60993b8edc3e92ea85bd913d42f49c6b3128")]
 [assembly: System.Reflection.AssemblyProductAttribute("VillaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VillaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
