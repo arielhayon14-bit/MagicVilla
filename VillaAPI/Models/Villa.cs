66c6c9d5bb09cb7a9b355288efc8b8882061afe6
ArielHayon <arielhayon14@gmail.com>
2025-12-03 13:29:06 +0200

    start working with DB

--- VillaAPI/Models/Villa.cs
+++ VillaAPI/Models/Villa.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace VillaAPI.Models
 {
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Details { get; set; }
